691110431d24a975e2b9bf19f431f9828f4ca67c
404: CTO_not_found <ebukauche52@gmail.com>
2023-01-07 21:55:08 +0100

    fixed content display

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ document.onreadystatechange = () => {
         console.log(response.data.results);
         let content = "";
         response.data.results.map(item => {
-          content += `<div class="content"><img src="./imgs/placeholder.png" class="image"><span class="name">${item.name}</span></div>`
+          content += `<div class="content"><div class="image-holder"><img src="./imgs/placeholder.png" class="image"></div><div class="name">${item.name}</div></div>`
         })
         
         document.getElementById('loading').style = 'display:none'
