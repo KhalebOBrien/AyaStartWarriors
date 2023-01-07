c6824f4ba01eeb9238a994cc15c59139fcc2ebce
404: CTO_not_found <ebukauche52@gmail.com>
2023-01-07 23:01:03 +0100

    show data on click of name

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ You are to create a SPA (single-page application) using HTML, CSS, and JavaScrip
 1. Display a dummy image for all single characters. [done]
 
 2. Make an HTTP request to populate your frontend application using the API
-    - Display a list with the names of each person from the API request.
+    - Display a list with the names of each person from the API request. [done]
     - On Clicking any name, display the details of that user on the screen, with name, gender, and height only.
 
 3. All implementations should be done using es6. [done]
--- js/main.js
+++ js/main.js
@@ -7,14 +7,21 @@ document.onreadystatechange = () => {
         const response = await axios.get(`/people/?page=${page}`)
         if (!response.data) return { code: 500 }
 
-        console.log(response.data.results);
-        let content = "";
+        let content = ""
         response.data.results.map(item => {
-          content += `<div class="content"><div><img src="https://via.placeholder.com/100x100.png/E7EAF4/262626?text=${item.name}" class="image"></div><div class="name">${item.name}</div></div>`
+          content += `<div class="content"><div><img src="https://via.placeholder.com/100x100.png/E7EAF4/262626?text=${item.name.split(' ').at(0)}" class="image"></div><div class="name">${item.name}</div><div class="data-box" style="display:none;"><b>Name:</b> ${item.name}<br><b>Gender:</b> ${item.gender}<br><b>Height:</b> ${item.height}</div></div>`
         })
         
         document.getElementById('loading').style = 'display:none'
         document.getElementById('main').innerHTML += content
+
+        const names = document.querySelectorAll('.name')
+        names.forEach(box => {
+          box.addEventListener('click', (event) => {
+            box.closest('div.content').querySelector('.data-box').removeAttribute('style')
+            box.setAttribute('style', 'display:none;')
+          });
+        });
         return { code: 200 }
       } catch (error) {
         console.error(error)
@@ -22,5 +29,6 @@ document.onreadystatechange = () => {
     }
 
     people(1)
+
   }
 }
