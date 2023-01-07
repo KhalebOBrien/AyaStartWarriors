9c82328b0c4a97b1b5af3b8091e2c865d01fed3c
404: CTO_not_found <ebukauche52@gmail.com>
2023-01-07 21:01:05 +0100

    fetch first 10 persons data

---
+++ js/main.js
@@ -0,0 +1,25 @@
+axios.defaults.baseURL = 'https://swapi.dev/api'
+
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    const people = async (page = 1) => {
+      try {
+        const response = await axios.get(`/people/?page=${page}`)
+        if (!response.data) return { code: 500 }
+
+        console.log(response.data.results);
+        let content = "";
+        response.data.results.map(item => {
+          content += `<div class="content"><img src="./imgs/placeholder.png" class="image"><span class="name">${item.name}</span></div>`
+        })
+    
+        document.getElementById('main').innerHTML += content
+        return { code: 200 }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    people(1)
+  }
+}
